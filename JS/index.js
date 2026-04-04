db5aee847ba4115bb1a810a0209e1d4022b15909
INGDurand <durandsipeuwou7@gmail.com>
2026-04-04 21:49:28 +0100

    ceci est la version a 50% responsive

---
+++ JS/index.js
@@ -0,0 +1,22 @@
+// alert("attention");
+let nom = "durand";
+let age = 19;
+let estungarcon=true;
+let estmajeur=true;
+// console.log (`Je suis ${nom} , j'ai ${age}ans et je suis un garcon; ${estungarcon}`) a
+if (age>=20) {
+    estmajeur=false
+    console.log(estmajeur)
+}
+// else {
+//     estmajeur=true
+    console.log(estmajeur)
+// }
+let nombre= 16;
+if (nombre %2==0) {
+   console.log ("le nombre est paire")
+}
+else {
+    console.log ("le nombre est impaire")
+}
+
